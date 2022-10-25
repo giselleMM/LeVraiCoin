8f515bd7f1cc1fe849379fd005344de168256d32
GiselleMM <giselle.magalong@gmail.com>
2022-10-25 16:45:14 +0200

    Add factory and tag fixtures

---
+++ src/src/Factory/PicturePostFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\PicturePost;
+use App\Repository\PicturePostRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<PicturePost>
+ *
+ * @method static PicturePost|Proxy createOne(array $attributes = [])
+ * @method static PicturePost[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static PicturePost[]|Proxy[] createSequence(array|callable $sequence)
+ * @method static PicturePost|Proxy find(object|array|mixed $criteria)
+ * @method static PicturePost|Proxy findOrCreate(array $attributes)
+ * @method static PicturePost|Proxy first(string $sortedField = 'id')
+ * @method static PicturePost|Proxy last(string $sortedField = 'id')
+ * @method static PicturePost|Proxy random(array $attributes = [])
+ * @method static PicturePost|Proxy randomOrCreate(array $attributes = [])
+ * @method static PicturePost[]|Proxy[] all()
+ * @method static PicturePost[]|Proxy[] findBy(array $attributes)
+ * @method static PicturePost[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static PicturePost[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static PicturePostRepository|RepositoryProxy repository()
+ * @method PicturePost|Proxy create(array|callable $attributes = [])
+ */
+final class PicturePostFactory extends ModelFactory
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        // TODO inject services if required (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services)
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
+            'photo' => self::faker()->text(),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+        return $this
+            // ->afterInstantiate(function(PicturePost $picturePost): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return PicturePost::class;
+    }
+}
