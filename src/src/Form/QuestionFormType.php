cfa7910e23b93bcea36ac97f08e84648ff0cbb48
Florent Paris <florentparis28@gmail.com>
2022-10-30 15:23:37 +0100

    style question post twig

--- src/src/Form/QuestionFormType.php
+++ src/src/Form/QuestionFormType.php
@@ -8,17 +8,24 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class QuestionFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Question:', TextareaType::class, [
+            ->add('label', TextareaType::class, [
                 'required' => true,
                 'attr' =>[
                     'placeholder' => "Write here your question...",
-                    'class' => "bg-white w-100"
+                    'class' => "bg-white w-100 border rounded"
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label'=> "Post Question",
+                'attr' => [
+                    'class' => "btn btn-warning text-white rounded"
                 ]
             ])
         ;
